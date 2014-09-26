eacd8917d026c9ca2d05ff3e8b5ab301dd659bd2
Travis CI <travis@example.com>
2014-09-26 11:18:46 +0000

    Deploy to GitHub Pages

---
+++ js/style.all.js
@@ -0,0 +1,52 @@
+(function() {
+  (function($) {
+    var Plugin, Site;
+    Site = (function() {
+      function Site(element, options) {
+        ({
+          DEFAULTS: {
+            mask: false
+          }
+        });
+        this.$element = $(element);
+        this.options = $.extend({}, DEFAULTS, options);
+      }
+
+      Site.prototype.toggle = function(show) {};
+
+      Site.prototype.show = function() {
+        return toggle(true);
+      };
+
+      Site.prototype.hide = function() {
+        return toggle(false);
+      };
+
+      return Site;
+
+    })();
+    Plugin = function(option) {
+      return this.each(function() {
+        var $this, data, options;
+        $this = $(this);
+        data = $this.data('bs.site');
+        options = typeof option === 'object' && option;
+        if (!data) {
+          data = new Button(this, options);
+          $this.data('bs.site', data);
+        }
+        if (option === 'toggle') {
+          return data.toggle;
+        } else if (option) {
+          return data.setState(option);
+        }
+      });
+    };
+    $.fn.button = Plugin;
+    return $.fn.button.Constructor = Button;
+  })(jQuery);
+
+}).call(this);
+
+
+//# sourceMappingURL=style.all.js.map
